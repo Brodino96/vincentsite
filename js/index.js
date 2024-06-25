a186c589f3dbe885377259eabe87fdfd29bc1405
Vincent Acanfora <brodino96@gmail.com>
2024-06-25 16:41:41 +0200

    Added function to redirect users

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
-window.onload = function() { // function to change page title when you leave the page
-    
-    const pageTitle = document.title; // original title of the page
+const pageTitle = document.title; // original title of the page
 
-    document.addEventListener("visibilitychange", function() { // when you leave or open the page
-        if (document.hidden) {
-            document.title = "Mi manchi, torna qui"
-        } else {
-            document.title = pageTitle
-        }
-    })
+document.addEventListener("visibilitychange", function() { // when you leave or open the page
+    if (document.hidden) {
+        document.title = "Mi manchi, torna qui"
+    } else {
+        document.title = pageTitle
+    }
+})
+
+function imageClicked (data) {
+
+    let url
+
+    switch (data) {
+        case "instagram":
+            url = "https://www.instagram.com/brodinostar_/"
+            break
+
+        case "github":
+            url = "https://github.com/Brodino96"
+            break
+    }
+
+    redirect(url)
+}
+
+function redirect (url) {
+    window.open(url, '_blank').focus()
 }
